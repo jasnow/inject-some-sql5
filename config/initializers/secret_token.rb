68643ed808053c6fa4f7ea2d2e530f493b85d6a9
Al Snow <jasnow@hotmail.com>
2016-06-14 10:01:03 -0400

    Clean up some errors

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BadSql::Application.config.secret_token = '02f648eb8338da8c60d5dc5618fc57fc063da825e89dc8814ffaac5a05248c95d4b8352a825a4d2015759e1493a0a22ae8d7901211086ce429e2dba6990e8d50'
+BadSql::Application.config.secret_token = ENV['SECRET_TOKEN']
